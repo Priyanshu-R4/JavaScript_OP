16e329f6d1e49dcba1f3e6036f2684bc979c0b37
Priyanshu Ranjan <priyanshu82ranjan48@gmail.com>
2025-08-13 21:17:30 +0530

    Lecture 1
    Lecture 1   - JavaScript -  Apna JavaScript

---
+++ Lecutre 1/hello.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+//here is our first code  Hello world
+
+//here we are sayin to compiler that on console give  a log  
+//log means print in below 
+
+//day 1 
---
+++ Lecutre 1/printing.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+console.log("Ranjan OP"); 
+
+console.log(5+7); //can print directly like using values directly 
+console.log("5+7") //this is normal sentence  because it has "" in it 
+
+StringName="Ranjan op" // variables are made like this 
+console.log(StringName) //here we printed a vriable of data type string 
+
+//day 1 
