a8dc9d3edef7f45fdec4b8cef9786586096a012f
Priyanshu Ranjan <priyanshu82ranjan48@gmail.com>
2025-08-15 17:51:43 +0530

    Create 1_comments.js

---
+++ Lecture 2/1_comments.js
@@ -0,0 +1,16 @@
+ex="while we write something and sometimes we need to explain things or mention for future reference"
+//so for that we use commnets like this with // 
+// and if u need comments for multiple lines , then use the next exmaple above
+
+/*
+here 
+easyily
+we 
+can
+make
+multi line comment
+in
+this 
+wasy
+ */
+//we can easily explain for reference or future documentation
