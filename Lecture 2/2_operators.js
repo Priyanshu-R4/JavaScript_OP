703b727be83ec1e1a80c3e79f7f20c6339c3ef90
Priyanshu Ranjan <priyanshu82ranjan48@gmail.com>
2025-08-15 21:26:46 +0530

    Create 2_operators.js

---
+++ Lecture 2/2_operators.js
@@ -0,0 +1,17 @@
+//this is used to perform some operation on data
+
+// Arithmetic Operators in JavaScript
+
+console.log(2 + 22, "Addition");
+console.log(2 - 22, "Subtraction");
+console.log(2 * 22, "Multiplication");
+console.log(22 / 2, "Division");
+console.log(2 % 22, "Modulus (Remainder)");
+console.log(2 ** 5, "Exponentiation (2 to the power 5)");
+
+// Increment and Decrement need variables
+let a = 1;
+console.log(++a, "Increment (prefix)"); // adds 1 before using
+console.log(a++, "Increment (postfix)"); // uses then adds 1
+console.log(--a, "Decrement (prefix)");
+console.log(a--, "Decrement (postfix)");
