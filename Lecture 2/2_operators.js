ec5c6c5aaced7fb0e7b6f424a96896c487c5fcb0
Priyanshu Ranjan <priyanshu82ranjan48@gmail.com>
2025-08-16 09:11:05 +0530

    Update 2_operators.js

--- Lecture 2/2_operators.js
+++ Lecture 2/2_operators.js
@@ -9,9 +9,21 @@ console.log(22 / 2, "Division");
 console.log(2 % 22, "Modulus (Remainder)");
 console.log(2 ** 5, "Exponentiation (2 to the power 5)");
 
-// Increment and Decrement need variables
+// Increment and Decrement need variables //also calleed as unary operators 
 let a = 1;
 console.log(++a, "Increment (prefix)"); // adds 1 before using
 console.log(a++, "Increment (postfix)"); // uses then adds 1
 console.log(--a, "Decrement (prefix)");
 console.log(a--, "Decrement (postfix)");
+
+//asignment operators 
+a= 12;// firs is =  for assignment 
+console.log("a");
+console.log(a+=15) //this means a=a+15
+console.log(a-=15) //this means a=a-15
+console.log(a*=15) //this means a=a*15
+console.log(a/=15) //this means a=a/15
+console.log(a%=15) //this means a=a%15
+console.log(a**=15) //this means a=a**15 (15*15*15 aisa 15 bar )
+
+
